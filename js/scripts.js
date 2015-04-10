2022d307d28420cbfb3086c6b844a49799c61a31
Jill Kuchman <jillkuchman@gmail.com>
2015-04-10 09:56:08 -0700

    Add html skeleton file

--- js/scripts.js
+++ js/scripts.js
@@ -21,17 +21,6 @@ function triangle(side_a, side_b, side_c) {
     else {
         type_of_triangle = "invalid";
     }
-    
+
     return type_of_triangle;
 }
-
-
-
-
-//input: the lengths of the sides of a triangle
-//output: whether they form an equilateral, isoceles, or scalene triangle
-//equilateral: a === b === c
-//isoceles: (a === b && !(b === c)) || (a === c && !(a === b))
-//scalene: a !== b !== c
-//they have to make a valid triangle!!
-//triangle inequality theorem: a+b>c, a+c>b, b+c>a
