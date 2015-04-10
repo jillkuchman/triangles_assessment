e6bbeb2b2982a51ee72765392d99a01950fd94b6
Jill Kuchman <jillkuchman@gmail.com>
2015-04-10 11:13:49 -0700

    Fix scalene and isoceles mix up

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 function triangle(side_a, side_b, side_c) {
     var type_of_triangle;
 
-    if((side_a + side_b > side_c) && (side_a + side_c > side_b) && (side_b + side_c > side_a)){
-        if((side_a === side_b) && (side_a === side_c)) {
+    var side_lengths = [side_a, side_b, side_c];
+
+    var sorted = side_lengths.sort();
+    console.log(sorted);
+
+    if(parseInt(sorted[0]) + parseInt(sorted[1]) > parseInt(sorted[2])) {
+        if  ( (parseInt(sorted[0]) === parseInt(sorted[1])) && (parseInt(sorted[0]) === parseInt(sorted[2])) ) {
             type_of_triangle = "equilateral";
         }
-        else if ((side_a === side_b) && (side_a !== side_c)) {
-            type_of_triangle = "scalene";
-        }
-        else if ((side_a === side_c) && (side_a !== side_b)) {
-            type_of_triangle = "scalene";
-        }
-        else if ((side_b === side_c) && (side_a !== side_c)) {
-            type_of_triangle = "scalene";
+        else if (
+            ((parseInt(sorted[0]) === parseInt(sorted[1])) && (parseInt(sorted[0]) !== parseInt(sorted[2])))
+            ||
+            ((parseInt(sorted[1]) === parseInt(sorted[2])) && (parseInt(sorted[0]) !== parseInt(sorted[2])))
+        ) {
+            type_of_triangle = "isoceles";
         }
         else {
-            type_of_triangle = "isoceles";
+            type_of_triangle = "scalene";
         }
     }
     else {
@@ -27,24 +30,26 @@ function triangle(side_a, side_b, side_c) {
 
 $(document).ready(function() {
     $("form#triangle_input").submit(function(event) {
-        var user_side_a = $("input#side_a").val();
-        var user_side_b = $("input#side_b").val();
-        var user_side_c = $("input#side_c").val();
-        var user_triangle_type = triangle(user_side_a, user_side_b, user_side_c);
+        var user_side_a = parseInt($("input#side_a").val());
+        var user_side_b = parseInt($("input#side_b").val());
+        var user_side_c = parseInt($("input#side_c").val());
+
+        var result = triangle(user_side_a, user_side_b, user_side_c);
 
         $("#result").show();
         $(".side_a").text(user_side_a);
         $(".side_b").text(user_side_b);
         $(".side_c").text(user_side_c);
+        $("#triangle_type").text(result);
 
-        if (user_triangle_type === "scalene"){
+
+        if (result === "scalene"){
             $(".an_display").text("");
         }
         else {
             $(".an_display").text("n");
         }
 
-        $("#triangle_type").text(user_triangle_type);
 
         event.preventDefault();
     });
